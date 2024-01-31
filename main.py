f0751e47367165e583bff16ad103148dbeec2551
Andre <ender07.70redne@gmail.com>
2024-01-31 16:42:44 +0300

    масштабирование

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 import os
 import sys
 
@@ -20,6 +21,12 @@ class Map(QMainWindow):
         self.maplabel = QLabel(self)
         self.maplabel.setGeometry(0, 0, self.width(), self.height())
         self.maplabel.setPixmap(self.get_map())
+        self.buttonup = QPushButton('-')
+        self.buttonup.clicked.connect(self.up)
+        self.buttonup.setGeometry(self.width() - 10, 10, 10, 10)
+        self.buttondown = QPushButton('+')
+        self.buttondown.clicked.connect(self.down)
+        self.buttondown.setGeometry(self.width() - 10, 25, 10, 10)
 
     def get_map(self):
         response = requests.get(server, params=par)
@@ -28,8 +35,25 @@ class Map(QMainWindow):
             file.write(response.content)
         return QPixmap(self.map_file).scaled(self.maplabel.width(), self.maplabel.height())
 
-#    def keyPressEvent(self, event):
-#        if event.
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.buttonup.click()
+        if event.key() == Qt.Key_PageDown:
+            self.buttondown.click()
+
+    def up(self):
+        oldraz = float(par['spn'].split(",")[0])
+        razm = oldraz * 1.5
+        if razm < 80:
+            par['spn'] = f'{razm},{razm}'
+            self.maplabel.setPixmap(self.get_map())
+
+    def down(self):
+        oldraz = float(par['spn'].split(",")[0])
+        razm = oldraz / 1.5
+        if razm > 0.001:
+            par['spn'] = f'{razm},{razm}'
+            self.maplabel.setPixmap(self.get_map())
 
     def closeEvent(self, event):
         os.remove(self.map_file)
