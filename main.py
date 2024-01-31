4237e25165d7c85367ec1d2313d4e34ac2a33ef6
Andre <ender07.70redne@gmail.com>
2024-01-31 16:12:51 +0300

    ШКОЛА НОМЕР 2 НЕ СПУТНИК

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import requests
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtGui import QPixmap
 import os
 import sys
 
 
 server = 'https://static-maps.yandex.ru/1.x/?'
-par = {"ll": '0,0',
-       'spn': '80,80',
-       'l': 'sat',
+par = {"ll": '40.692065,55.615141',
+       'spn': '0.001,0.001',
+       'l': 'map',
        'size': '400,400'}
 
 
 class Map(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(100, 100, 600, 600)
+        self.setGeometry(100, 100, 800, 800)
         self.setWindowTitle('Yandex maps')
         self.maplabel = QLabel(self)
-        self.maplabel.setGeometry(0, 0, 600, 600)
+        self.maplabel.setGeometry(0, 0, self.width(), self.height())
         self.maplabel.setPixmap(self.get_map())
 
     def get_map(self):
@@ -26,7 +26,10 @@ class Map(QMainWindow):
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
-        return QPixmap(self.map_file).scaled(600, 600)
+        return QPixmap(self.map_file).scaled(self.maplabel.width(), self.maplabel.height())
+
+#    def keyPressEvent(self, event):
+#        if event.
 
     def closeEvent(self, event):
         os.remove(self.map_file)
